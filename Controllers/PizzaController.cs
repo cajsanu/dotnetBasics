22cce9a51d25040760b369a2cbb795ee9794b827
Cajsa Nummelin <cajsa.nummelin@gmail.com>
2024-07-16 09:30:44 +0300

    Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,69 @@
+using dotnetBasics.Models;
+using dotnetBasics.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetBasics.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+    // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
+
+    // PUT action
+    [HttpPut]
+    public IActionResult Update(Pizza pizza, int id)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    // DELETE action
+    [HttpDelete]
+    public IActionResult Remove(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
+}
